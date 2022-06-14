da448c35e4e322ba7e2e5ed5a8ce268f2c7bc429
Matt Skone <matt.skone@gmail.com>
2022-06-14 09:14:44 -0700

    Sync dependency versions

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=["earthengine-api>=0.1.303"],
+    install_requires=["earthengine-api>=0.1.312"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
